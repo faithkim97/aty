d2ebb2625032fa9981a6a84b6233a1bf430dfdd0
Faith <faithkim1158@gmail.com>
2018-01-05 12:29:49 -0500

    editor not really working--dialogue

--- Assets/Scripts/Dialogue/DialogueEditor.cs
+++ Assets/Scripts/Dialogue/DialogueEditor.cs
@@ -4,32 +4,63 @@ using UnityEngine;
 using UnityEditor;
 
 public class DialogueEditor : EditorWindow {
-    Rect window1;
-    Rect window2;
+    /// <summary>
+    /// keep track of what to show in Dialogue Editor
+    /// </summary>
+    List<Rect> windows = new List<Rect>();
+    /// <summary>
+    /// keep track of neighborless windows to attach
+    /// </summary>
+    List<int> windowsToAttach = new List<int>();
+    /// <summary>
+    /// keep track of windows that have neighbors
+    /// </summary>
+    List<int> attachedWindows = new List<int>();
+    [SerializeField]
+    [HideInInspector]
+    private DialogueTree tree;
+    public string dialogue = "ho there!";
 
     [MenuItem("Window/Dialogue Editor")]
     static void ShowEditor() {
         DialogueEditor editor = EditorWindow.GetWindow<DialogueEditor>();
-        editor.Init();
     }
 
-     public void Init() {
-        window1 = new Rect(10, 10, 100, 100);
-        window2 = new Rect(210, 210, 100, 100);
-    } s
-
     private void OnGUI() {
+        if (windowsToAttach.Count == 2) {
+            attachedWindows.Add(windowsToAttach[0]);
+            attachedWindows.Add(windowsToAttach[1]);
+            windowsToAttach = new List<int>();
+        }
 
-       // if (windowsToAttach.Count )
-        DrawNodeCurve(window1, window2); //curve drawn
+        if (attachedWindows.Count >= 2) {
+            for (int i = 0; i < attachedWindows.Count; i += 2) {
+                DrawNodeCurve(windows[attachedWindows[i]], windows[attachedWindows[i + 1]]);
+            }
+        }
+
         BeginWindows();
-        window1 = GUI.Window(1, window1, DrawNodeWindow, "Window 1");
-        window2 = GUI.Window(2, window2, DrawNodeWindow, "Window 2");
-        EndWindows(); 
+        if (GUILayout.Button("Create Node")) {
+           
+            windows.Add(new Rect(10, 10, 100, 100));
+        }
 
+        for (int i = 0; i < windows.Count; i++) {
+            windows[i] = GUI.Window(i, windows[i], DrawNodeWindow, "Window " + i);
+           
+        }
+        EndWindows();
     }
 
     void DrawNodeWindow(int id) {
+        if (GUILayout.Button("Attach")) {
+            windowsToAttach.Add(id);
+        }
+
+        if (GUILayout.Button("Add Dialogue")) {
+            dialogue = GUI.TextArea(new Rect(10, 10, 100, 100), dialogue, 25);
+            tree.getCurrNode().setDialogue(dialogue);
+        }
         GUI.DragWindow();
     }
 
@@ -39,10 +70,11 @@ public class DialogueEditor : EditorWindow {
         Vector3 startTan = startPos + Vector3.right * 50;
         Vector3 endTan = endPos + Vector3.left * 50;
         Color shadowCol = new Color(0, 0, 0, 0.06f);
-        for (int i = 0; i < 3; i++) //draw shadow
+
+        for (int i = 0; i < 3; i++) {// Draw a shadow
             Handles.DrawBezier(startPos, endPos, startTan, endTan, shadowCol, null, (i + 1) * 5);
-        
+        }
+
+        Handles.DrawBezier(startPos, endPos, startTan, endTan, Color.black, null, 1);
     }
-
-
 }
