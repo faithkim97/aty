bc087b50804bc47bff443a09c56370d930354106
Faith <faithkim1158@gmail.com>
2018-02-08 20:40:01 -0500

    need to test DialogueCreator

--- Assets/Scripts/Dialogue/DialogueCreator.cs
+++ Assets/Scripts/Dialogue/DialogueCreator.cs
@@ -19,7 +19,9 @@ public class DialogueCreator : EditorWindow {
 
     private void OnGUI() {
         attachedObject = (GameObject)EditorGUILayout.ObjectField("Game Object to Attach Dialogue", attachedObject, typeof(GameObject), true);
-        dialogueList = attachedObject.GetComponent<DialogueList>();
+        if (attachedObject != null) {
+            dialogueList = attachedObject.GetComponent<DialogueList>();
+        }
         if (attachedWindows.Count >= 2) {
             for (int i = 0; i < attachedWindows.Count; i += 2) {
                 DrawNodeCurve(windows[attachedWindows[i]], windows[attachedWindows[i + 1]]);
@@ -35,6 +37,15 @@ public class DialogueCreator : EditorWindow {
             dialogue.Add("insert dialogue here");
         }
 
+        if (GUILayout.Button("Save Dialogue List")) {
+            dialogueList.SaveDialogueList(dialogue);
+            Debug.Log("saved");
+        }
+
+        if (GUILayout.Button("Clear Dialogue List")) {
+            dialogueList.ClearDialogue();
+        }
+
     }//end of onGUI
 
 
@@ -44,9 +55,18 @@ public class DialogueCreator : EditorWindow {
             dialogueList.setDialogue(dialogue[id]);
         }
 
+        if (GUILayout.Button("Attach")) {
+            windowsToAttach.Add(id);
+            if (windowsToAttach.Count == 2) {
+                attachedWindows.Add(windowsToAttach[0]);
+                attachedWindows.Add(windowsToAttach[1]);
+                windowsToAttach = new List<int>();
+            }//end of count == 2
+        }
 
 
-    }
+
+    }//end of DrawNodeWindow
 
     void DrawNodeCurve(Rect start, Rect end) {
         Vector3 startPos = new Vector3(start.x + start.width, start.y + start.height / 2, 0);
