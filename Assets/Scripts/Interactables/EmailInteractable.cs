f01c9a59d45016beac595fe240953b7e81714a1a
faithkim97 <Faith@Faiths-MacBook-Air.local>
2018-03-16 18:48:33 -0400

    created emailinteractable script

---
+++ Assets/Scripts/Interactables/EmailInteractable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Makes UI of emails/texts interactable 
+/// </summary>
+public class EmailInteractable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if x, then exit
+		if (Input.GetKeyDown(KeyCode.X)) {
+			Image emailImage = gameObject.GetComponent<Image> ();
+			emailImage.enabled = false;
+		} 
+	}
+}//end of EmailInteractable
