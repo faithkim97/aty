46e4e8ece9f61112413e5e1a338349241e052bc4
Faith <faithkim1158@gmail.com>
2018-03-18 19:15:44 -0400

    fadesprite() function works smoothly

--- Assets/Scripts/Interactables/ChangeImage.cs
+++ Assets/Scripts/Interactables/ChangeImage.cs
@@ -16,17 +16,9 @@ public class ChangeImage : MonoBehaviour {
 		sr = parent.GetComponent<SpriteRenderer> ();
 	}
 
-//	void Update() {
-//		if (triggered) {
-//			FadeSprite ();
-//		}
-//	}
-
 	void OnTriggerEnter2D(Collider2D col) {
 		//if triggered, then change the bombs to other image 
 		if (col.gameObject.CompareTag("player")) {
-			Debug.Log ("triggered");
-			//triggered = true;
 			StartCoroutine(FadeSprite());
 		}//end of if
 
@@ -34,22 +26,20 @@ public class ChangeImage : MonoBehaviour {
 
 	private IEnumerator FadeSprite() {
 		//sr.sprite = changeTo;
-		Color tmp = sr.color; 
-		tmp.a = 1.0f;
+		Color tmp = sr.color;
 		//fade out
-		for (float i = 1.0f; i >= 0.0f; i -= 0.05f) {
-			sr.color = tmp;
-			tmp.a -= i;
-			yield return null;
+		for (float i = tmp.a; i > 0.0f; i -= 0.05f) {
+            sr.color = tmp;
+			tmp.a = i;
+            yield return null;
 		}
 		sr.sprite = changeTo;
 		tmp.a = 0.0f;
 		//fade in 
-		for (float i = 0.0f; i <= 1.0f; i += 0.05f) {
+		for (float i = tmp.a; i <= 1.0f; i += 0.05f) {
 			sr.color = tmp;
-			tmp.a += i;
+			tmp.a = i;
 			yield return null;
-		}
-		//triggered = false;
+		} 
 	}//end of FadeSprite
 }
