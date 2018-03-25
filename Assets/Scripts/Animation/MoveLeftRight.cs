620f7721559ac6c100032820b8e2c18b196ad239
Faith <faithkim1158@gmail.com>
2018-03-25 14:57:01 -0400

    got rid of box colliders on obstacles

--- Assets/Scripts/Animation/MoveLeftRight.cs
+++ Assets/Scripts/Animation/MoveLeftRight.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveLeftRight: MonoBehaviour {
-	///number of steps to take to move left and right
-	[SerializeField]
-	private int numSteps = 5; 
+    ///number of steps to take to move left and right
+    [SerializeField]
+    private float numSteps;
 
 	[SerializeField]
 	private float moveSpeed;
@@ -22,7 +22,6 @@ public class MoveLeftRight: MonoBehaviour {
 	private IEnumerator MoveRight() {
 		//Debug.Log ("inside move right");
 		for (float i = 0.0f; i < numSteps; i += Time.deltaTime) {
-            Debug.Log("i = " + i);
 			gameObject.transform.position = new Vector3 (gameObject.transform.position.x + moveSpeed *Time.deltaTime , gameObject.transform.position.y, gameObject.transform.position.z);
             yield return new WaitForSeconds(0.05f);
 		}//end of for        
