f9a6fb9ea1c129e115b2f4ca21f2f750bd1247a4
Faith <faithkim1158@gmail.com>
2018-03-30 21:11:00 -0400

    added noninteractble npc

---
+++ Assets/Scripts/Dialogue/NonInteractDialogue.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NonInteractDialogue : MonoBehaviour {
+    public Text text;
+    [SerializeField]
+    private string dialogue;
+
+    private void OnTriggerStay2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("player")) {
+              ActivateDialogue();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("player")) {
+            DeactivateDialogue();
+        }
+    }
+
+    private void ActivateDialogue() {
+        text.text = dialogue;
+    }
+
+    private void DeactivateDialogue() {
+        text.text = "";
+    }
+
+}//end of NonInteractDialogue
