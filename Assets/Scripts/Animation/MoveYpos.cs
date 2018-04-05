0d2c9ce291d495e9bf1096c6870b5fa9a64e1657
Faith <faithkim1158@gmail.com>
2018-04-05 11:10:14 -0400

    added MoveYpos

---
+++ Assets/Scripts/Animation/MoveYpos.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveYpos : MonoBehaviour {
+
+    private Rigidbody2D rb;
+
+    public  float numSteps = 0.0f;
+    public float moveSpeed;
+
+
+
+    private IEnumerator Start() {
+        while (true) {
+            yield return StartCoroutine(MoveUp());
+            yield return StartCoroutine(MoveDown());
+        }
+    }
+
+    public IEnumerator MoveUp() {
+        //Debug.Log ("inside move right");
+        for (float i = 0.0f; i < numSteps; i += Time.deltaTime) {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + moveSpeed * Time.deltaTime, gameObject.transform.position.z);
+            yield return new WaitForSeconds(0.05f);
+        }//end of for      
+    }
+
+    public IEnumerator MoveDown() {
+        for (float i = 0.0f; i < numSteps; i += Time.deltaTime) {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - moveSpeed * Time.deltaTime, gameObject.transform.position.z);
+            yield return new WaitForSeconds(0.05f);
+        }//end of for  
+    }
+
+
+
+}//end of MoveYpos
