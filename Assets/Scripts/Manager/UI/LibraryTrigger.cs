5607b447e8d2cc41b6b466788359b45145b8daf2
Faith <faithkim1158@gmail.com>
2018-04-19 02:10:43 -0400

    committing

---
+++ Assets/Scripts/Manager/UI/LibraryTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class LibraryTrigger : MonoBehaviour {
+    public Text text;
+
+    void OnTriggerStay2D(Collider2D col) {
+        if (col.gameObject.CompareTag("player")) {
+            text.enabled = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col) {
+        if (col.gameObject.CompareTag("player")) {
+            text.enabled = false;
+        }
+    }
+}//end of library trigger
