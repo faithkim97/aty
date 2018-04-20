5a3bb23d90672667db08688787bc153484aa24f8
Faith <faithkim1158@gmail.com>
2018-04-20 07:53:37 -0400

    added instructions to dialogue

---
+++ Assets/Scripts/Manager/UI/NPCPressF.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPCPressF : MonoBehaviour {
+    public Text pressF;
+    private DialogueManager dMan;
+
+    private void Start() {
+        dMan = GameObject.FindObjectOfType<DialogueManager>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("player")) {
+            pressF.enabled = true;
+        }
+
+       
+    }
+
+    private void OnTriggerStay2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("player")) {
+            if (dMan.diaActive) {
+                Debug.Log("diaActive");
+                pressF.enabled = false;
+            }
+        }
+    }//end of stay
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("player")) {
+            pressF.enabled = false;
+        }
+    }//end of exit
+
+}//end of NPCPressF
